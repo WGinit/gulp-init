ab934f5db97910be6b4f07a5c25181412450dde7
白山羊 <drydingrui@gmail.com>
2017-12-25 14:40:46 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -120,32 +120,3 @@ gulp.task('serve', ['clean'], function () {
 });
 
 gulp.task('default', ['serve']);
-
-
-
-
-// //监视文件的变化
-// gulp.task("watch", function () {
-//     gulp.watch("src/assets/less/*.less", ['less']);
-// });
-
-// gulp.task('build', function () {
-//     gulp.src(['index.html'])
-//         .pipe(htmlbuild({
-//             // build js with preprocessor 
-//             js: htmlbuild.preprocess.js(function (block) {
-
-//                 // read paths from the [block] stream and build them 
-//                 // ... 
-
-//                 // then write the build result path to it 
-//                 block.write('buildresult.js');
-//                 block.end();
-
-//             })
-//         }))
-//         .pipe(gulp.dest('./build'));
-// });
-
-
-// gulp.task('default', ['less','watch']);
